d8ec65052f0ed27dd940c1fc160aa6c5f347503f
Muhammad Hasan <muhammadhasan50@gmail.com>
2020-03-22 19:56:55 +0700

    Add Unit Testing and Integration Testing

---
+++ Kalkulator/test/ekspresi/akarEkspresiIT.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ekspresi;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author USER
+ */
+public class akarEkspresiIT {
+    
+    public akarEkspresiIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getHasil method, of class akarEkspresi.
+     */
+    @Test
+    public void testGetHasil() {
+        System.out.println("getHasil");
+        akarEkspresi instance = new akarEkspresi();
+        double expResult = 0.0;
+        double result = instance.getHasil();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
